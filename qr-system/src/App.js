dfe1b3393971b06dcfce11dbf0088092172acdcf
kamaucyprian12 <kamaucyprian12@gmail.com>
2024-10-11 22:21:51 +0300

    dashboard update

--- qr-system/src/App.js
+++ qr-system/src/App.js
@@ -1,4 +1,3 @@
-// src/App.js
 import React from 'react';
 import Sidebar from './components/Sidebar';
 import LecDash from './components/LecView/LecDash';  // Import LecDash component
@@ -6,11 +5,11 @@ import LecDash from './components/LecView/LecDash';  // Import LecDash component
 function App() {
   return (
     <div className="app-container">  {/* Flex container for sidebar and main content */}
-      <Sidebar />  {/* Sidebar will stay on the left */}
-      
-      {/* Main content should appear next to the sidebar */}
+      <div className="sidebar-container">  
+        <Sidebar />  
+      </div>
       <div className="main-content">  
-        <LecDash />  {/* Dashboard content here */}
+        <LecDash />  
       </div>
     </div>
   );
