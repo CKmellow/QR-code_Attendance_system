0d9746dc135fedaf6fb3270635aa129bde1cd1df
kamaucyprian12 <kamaucyprian12@gmail.com>
2024-11-19 21:12:33 +0300

    Lec side responsiveness

--- qr-system/src/Pages/LecHome.js
+++ qr-system/src/Pages/LecHome.js
@@ -1,21 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import '../Pages/LecHome.css';
 import LecDash from "../components/LecView/LecDash";
 import Sidebar from "../components/Sidebar";
-
+import { FaBars } from "react-icons/fa";
 
 function LecHome() {
-    return (
-        <div className="app-container">  {/* Flex container for sidebar and main content */}
-          <div className="sidebar-container">  
-            <Sidebar />  
-          </div>
-          <div className="main-content">  
-            <LecDash />  
-            
-          </div>
-        </div>
-      );
+  const [isSidebarVisible, setIsSidebarVisible] = useState(true);
 
+  const toggleSidebar = () => {
+    setIsSidebarVisible(!isSidebarVisible);
+  };
+
+  return (
+    <div className="app-container">
+      <button className="sidebar-toggle" onClick={toggleSidebar}>
+        <FaBars />
+      </button>
+      <div className={`sidebar-container ${isSidebarVisible ? "" : "hidden"}`}>
+        <Sidebar />
+      </div>
+      <div className="main-content">
+        <LecDash />
+      </div>
+    </div>
+  );
 }
+
 export default LecHome;
