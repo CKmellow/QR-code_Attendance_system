7ea8062245f4c830ddba5c7e30aaab468256cc34
kamaucyprian12 <kamaucyprian12@gmail.com>
2024-11-19 21:56:35 +0300

    Lechome changes

--- qr-system/src/Pages/LecHome.js
+++ qr-system/src/Pages/LecHome.js
@@ -1,29 +1,21 @@
-import React, { useState } from "react";
+import React from "react";
 import '../Pages/LecHome.css';
 import LecDash from "../components/LecView/LecDash";
 import Sidebar from "../components/Sidebar";
-import { FaBars } from "react-icons/fa";
+
 
 function LecHome() {
-  const [isSidebarVisible, setIsSidebarVisible] = useState(true);
+    return (
+        <div className="app-container">  {/* Flex container for sidebar and main content */}
+          <div className="sidebar-container">  
+            <Sidebar />  
+          </div>
+          <div className="main-content">  
+            <LecDash />  
+            
+          </div>
+        </div>
+      );
 
-  const toggleSidebar = () => {
-    setIsSidebarVisible(!isSidebarVisible);
-  };
-
-  return (
-    <div className="app-container">
-      <button className="sidebar-toggle" onClick={toggleSidebar}>
-        <FaBars />
-      </button>
-      <div className={`sidebar-container ${isSidebarVisible ? "" : "hidden"}`}>
-        <Sidebar />
-      </div>
-      <div className="main-content">
-        <LecDash />
-      </div>
-    </div>
-  );
 }
-
 export default LecHome;
