6fa30ebff56488b85f722130b4f4c10cc0cdc9c7
kamaucyprian12 <kamaucyprian12@gmail.com>
2024-11-23 12:15:19 +0300

    login-page

--- qr-system/src/Pages/LoginPage.js
+++ qr-system/src/Pages/LoginPage.js
@@ -24,7 +24,7 @@ const LoginPage = () => {
     const loginData = { _id, password };
   
     try {
-      const response = await fetch('/login', {
+      const response = await fetch('https://qr-attendace-backend.onrender.com/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(loginData),
